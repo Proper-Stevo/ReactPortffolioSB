a035a35066a827dff303dea12875ce4c35194b91
Stevo <steven.barrios92@gmail.com>
2022-11-02 14:21:44 -0700

    fixed button issue on pofolio section

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+This React Portfolio was a great way to demonstrate 
 
 ## Table of Contents (Optional)
 
--- src/components/pages/Portfolio/Index.js
+++ src/components/pages/Portfolio/Index.js
@@ -11,107 +11,129 @@ export default function Portfolio() {
       < br />
       < hr />
       < br />
-    <Carousel>
-    <Carousel.Item interval={500}>
-        <img
-          className="d-block mx-auto w-50"
-          src="./img/systembade.png"
-          alt="Second slide"
-          height={"400px"}
-          width={'400px'}
-        />
-        <Carousel.Caption className='caption'>
-          <h3>Orbital Odyssey</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <button href="https://github.com/Proper-Stevo/ThePonderosaPines">GitHub</button>
-          <button href="https://proper-stevo.github.io/ThePonderosaPines/">Website</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img 
-          className="d-block mx-auto w-50"
-          src="./img/cards.webp"
-          alt="First slide"
-          height={"400px"}
-          width={'400px'}
-        />
-        <Carousel.Caption className='caption'>
-          <h3>Card Game</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          <button href="https://nameless-sierra-87070.herokuapp.com/notes">GitHub</button>
-          <button href="https://nameless-sierra-87070.herokuapp.com/notes">Website</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block mx-auto w-50"
-          src="./img/nationalPark.jpeg"
-          alt="Second slide"
-          height={"400px"}
-          width={'400px'}
-        />
-        <Carousel.Caption className='caption'>
-          <h3>National Park Finder</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <button href="https://github.com/Proper-Stevo/ThePonderosaPines">GitHub</button>
-          <button href="https://proper-stevo.github.io/ThePonderosaPines/">Website</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block mx-auto w-50"
-          src="./img/noteimage.png"
-          alt="Third slide"
-          height={"400px"}
-          width={'400px'}
-        />
-        <Carousel.Caption className='caption'>
-          <h3>Note Taker Applcation</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-          <button href="https://github.com/Proper-Stevo/NoteTaker">GitHub</button>
-          <button href="https://nameless-sierra-87070.herokuapp.com/notes">Website</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block mx-auto w-50"
-          src="./img/Calculator_PNG.png"
-          alt="Third slide"
-          height={"400px"}
-          width={'400px'}
-        />
-        <Carousel.Caption className='caption'>
-          <h3>Calculator</h3>
-          <p>
-            Very Simple Calculator
-          </p>
-          <button href='https://github.com/Proper-Stevo/calculator'>GitHub</button>
-          <button href='https://proper-stevo.github.io/calculator/'>Website</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block mx-auto w-50"
-          src="./img/forcast.png"
-          alt="Third slide"
-          height={"400px"}
-          width={'400px'}
-        />
-        <Carousel.Caption className='caption'>
-          <h3>Weather Guide</h3>
-          <p>
-            A Weather Guide that lets you select a city as an input and show's you the current weather and the next
-            5 day forcast as well! 
-          </p>
-          <button href='https://github.com/Proper-Stevo/WeatherGuide'>GitHub</button>
-          <button href='https://proper-stevo.github.io/WeatherGuide/'>Website</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <Carousel>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block mx-auto w-50"
+            src="./img/systembade.png"
+            alt="Second slide"
+            height={"400px"}
+            width={'400px'}
+          />
+          <Carousel.Caption className='caption'>
+            <h3>Orbital Odyssey</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <a href="https://github.com/Proper-Stevo/ThePonderosaPines">
+              <button >GitHub</button>
+            </a>
+            < a href="https://proper-stevo.github.io/ThePonderosaPines/">
+              <button >Website</button>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block mx-auto w-50"
+            src="./img/cards.webp"
+            alt="First slide"
+            height={"400px"}
+            width={'400px'}
+          />
+          <Carousel.Caption className='caption'>
+            <h3>Card Game</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            < a href="https://nameless-sierra-87070.herokuapp.com/notes">
+              <button >GitHub</button>
+            </a>
+            < a href="https://nameless-sierra-87070.herokuapp.com/notes">
+              <button >Website</button>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block mx-auto w-50"
+            src="./img/nationalPark.jpeg"
+            alt="Second slide"
+            height={"400px"}
+            width={'400px'}
+          />
+          <Carousel.Caption className='caption'>
+            <h3>National Park Finder</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            < a href="https://github.com/Proper-Stevo/ThePonderosaPines">
+              <button >GitHub</button>
+            </a>
+            < a href="https://proper-stevo.github.io/ThePonderosaPines/">
+              <button >Website</button>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto w-50"
+            src="./img/noteimage.png"
+            alt="Third slide"
+            height={"400px"}
+            width={'400px'}
+          />
+          <Carousel.Caption className='caption'>
+            <h3>Note Taker Applcation</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+            < a href="https://github.com/Proper-Stevo/NoteTaker">
+              <button >GitHub</button>
+            </a>
+            < a href="https://nameless-sierra-87070.herokuapp.com/notes">
+              <button >Website</button>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto w-50"
+            src="./img/Calculator_PNG.png"
+            alt="Third slide"
+            height={"400px"}
+            width={'400px'}
+          />
+          <Carousel.Caption className='caption'>
+            <h3>Calculator</h3>
+            <p>
+              Very Simple Calculator
+            </p>
+            < a href='https://github.com/Proper-Stevo/calculator'>
+              <button >GitHub</button>
+            </a>
+            < a href='https://proper-stevo.github.io/calculator/'>
+              <button >Website</button>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto w-50"
+            src="./img/forcast.png"
+            alt="Third slide"
+            height={"400px"}
+            width={'400px'}
+          />
+          <Carousel.Caption className='caption'>
+            <h3>Weather Guide</h3>
+            <p>
+              A Weather Guide that lets you select a city as an input and show's you the current weather and the next
+              5 day forcast as well!
+            </p>
+            < a href='https://github.com/Proper-Stevo/WeatherGuide'>
+              <button >GitHub</button>
+            </a>
+            < a href='https://proper-stevo.github.io/WeatherGuide/'>
+              <button >Website</button>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
-
-// the projects youve worked on 
